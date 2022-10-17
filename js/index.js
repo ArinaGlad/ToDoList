68c7b78c6127fd7001519caa6c1950575624f567
Arina Gladysheva <arina1110@mail.ru>
2022-10-17 23:57:21 +0300

    LS part 1

--- js/index.js
+++ js/index.js
@@ -3,11 +3,38 @@ const taskInput = document.querySelector('input[name="task"]');
 const buttonAdd = document.querySelector('.form-task__button');
 const listTask = document.querySelector('.task__list');
 const formSubmitTask = document.querySelector('.form-task');
+const taskText = document.querySelector('.task__text');
+
+let tasks = [];
+
+function addDoneClass (array) {
+    if (array.done) {
+        taskText.classList.add('task__text_done');
+    }
+}
 
 // добавление задачи
 function createtask () {
     const taskElement = taskTemplate.querySelector('.task__item').cloneNode(true);
+    const taskText = taskElement.querySelector('.task__text');
+
+    const newTask = {
+        id : "id" + Math.random().toString(16).slice(2),
+        text: taskInput.value,
+        done: false,
+    };
+
+    tasks.push(newTask);
+
     taskElement.querySelector('.task__text').textContent = taskInput.value;
+    taskElement.id = newTask.id;
+
+    // добавляем класс что задача сделана
+    if (newTask.done) {
+        taskText.classList.add('task__text_done');
+    }
+    
+
     return taskElement;
 }
 
@@ -28,7 +55,19 @@ function deleteTask (event) {
     if(!event.target.classList.contains('task__delete')) {
         return;
     }
-    event.target.closest('.task__item').remove();
+
+    const closestItem = event.target.closest('.task__item');
+    closestItem.remove();
+
+    const index = tasks.findIndex((item) => {
+        if (item.id == closestItem.id) {
+            return true;
+        }
+    });
+
+    // удаляем из массива
+    tasks.splice(index, 1);
+
     if (listTask.children.length === 2) {
         listTask.querySelector('.task__empty').classList.remove('task__empty_active');
     }
